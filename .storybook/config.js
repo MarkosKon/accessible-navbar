389b67fb2eb0182f9b6f052d0a2ccef51b4d7b22
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2019-05-23 15:57:33 +0300

    Added storybook a11y plugin.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,10 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from "@storybook/react";
+import { withA11y } from "@storybook/addon-a11y";
+
+addDecorator(withA11y);
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.(js|jsx)$/);
+const req = require.context("../stories", true, /.stories.(js|jsx)$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
